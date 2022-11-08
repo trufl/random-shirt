cf50cfd56cf461edfde2b8c6a5c8ad9e76d11a57
T <endlesstruffle80@gmail.com>
2022-11-07 21:09:07 -0700

    Adds model relationships

--- models/index.js
+++ models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: 'ProductTag' });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: 'ProductTag' });
 
 module.exports = {
   Product,
